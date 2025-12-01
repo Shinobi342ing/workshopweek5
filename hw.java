31f1fe610988403ec18b5dff6eeff2d270bd3e70
Ingghang <limbuingghang@gmail.com>
2025-12-01 14:19:42 +0545

    Initial sharing of project

---
+++ hw.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class hw here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class hw
+{
+    // instance variables - replace the example below with your own
+    public static void main(String args[]){
+        
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        
+    }
+}
